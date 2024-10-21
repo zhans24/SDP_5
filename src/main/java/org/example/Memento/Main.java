cf9e9461df4a7e3adf48dacb9d67bce34486bf76
zhanserik <zhasikgaziz@gmail.com>
2024-10-21 19:29:50 +0500

    final

--- src/main/java/org/example/Memento/Main.java
+++ src/main/java/org/example/Memento/Main.java
@@ -5,21 +5,17 @@ public class Main {
         TextEditor text = new TextEditor();
         History history = new History();
 
-        // Writing initial text
         text.write("Zhasik");
         history.save(text.save());
-        System.out.println("Current Text: " + text.getText()); // Output: Zhasik
-
-        // Writing more text
+        System.out.println("Current Text: " + text.getText());
         text.write(" hello");
         history.save(text.save());
-        System.out.println("Current Text: " + text.getText()); // Output: Zhasik hello
+        System.out.println(text.getText());
 
-        // Undoing the last text addition
         Memento last = history.undo();
         if (last != null) {
             text.restore(last);
         }
-        System.out.println("After Undo: " + text.getText()); // This should now show: Zhasik
+        System.out.println(text.getText());
     }
 }
